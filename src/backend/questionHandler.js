445cf31ea0de4c9026e7b2952bafc2fc0f686880
Jared <jaredluke3@gmail.com>
2025-09-13 16:36:48 +0200

    Cahnged prompt

--- src/backend/questionHandler.js
+++ src/backend/questionHandler.js
@@ -13,7 +13,7 @@ class QuestionSetHandler {
         const userLevel = stateContext.getLevel();
         const language = stateContext.getLanguage();
 
-        const prompt = ` Please generate a set of more than 5 but less than 11 coding questions for the ${language} programming language on a ${userLevel} level with some of them being multiple choice questions, others fill in the blank questions, and others being coding questions. The format of multiple choice questions should be as follows: {
+        const prompt = `Please generate a set of more than 5 but less than 11 coding questions for the ${language} programming language on a ${userLevel} level with some of them being multiple choice questions, others fill in the blank questions, and others being coding questions. The format of multiple choice questions should be as follows: {
             "id": 1,
             "type": "multiple-choice",
             "topic": "[Relevant topic, e.g., 'Data Structures']",
