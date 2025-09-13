cc68d19596157c14c2a6f60c8b48b7d39ad0c6a4
Jared <jaredluke3@gmail.com>
2025-09-13 13:15:35 +0200

    Add Google GenAI integration for dynamic question generation and update dependencies

---
+++ src/backend/questionHandler.js
@@ -0,0 +1,46 @@
+import dotenv from "dotenv";
+import { GoogleGenAI } from "@google/";genai
+
+dotenv.config();
+
+const { LevelContext, BeginnerState, IntermediateState, AdvancedState } = require('./levelState');
+
+class QuestionSetHandler {
+    construction(){
+        this.genAI = new GoogleGenAI(process.env.API_KEY);
+    }
+
+    async createQuestionSet(stateContext = new LevelContext()){
+        const userLevel = stateContext.getLevel();
+        
+        const prompt = `Please generate a set of more than 5 but less than 11 coding questions on a ${userLevel} level`
+        + ` with some of them being multiple choice questions, others fill in the blank questions, and others being coding questions. `
+        + ` and please return this in a JSON format. `
+        + ` The format of multiple choice questions should be as follows: {
+            "id": 1,
+            "type": "multiple-choice",
+            "topic": "[Relevant topic, e.g., 'Data Structures']",
+            "question": "[Full text of the question]",
+            "options": [
+                "A. [Option A]",
+                "B. [Option B]",
+                "C. [Option C]",
+                "D. [Option D]"
+            ],
+            "answer": "[Correct option letter, e.g., 'C']",
+            "explanation": "[Brief explanation of the correct answer and why other options are incorrect]"
+            }`
+        + `  `;
+
+        const response = await this.genAI.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: prompt,
+            config: {
+            thinkingConfig: {
+                thinkingBudget: 0,
+            },
+            }
+        });
+        console.log(response.text);
+    }
+}
